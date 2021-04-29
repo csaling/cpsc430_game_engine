603ac8b0cf7b4a9b77c959a782c1064a7e45f259
Cade Saling <csaling@muskingum.edu>
2021-04-29 16:08:45 -0400

    Fixed text error

--- Cade's Game Executable/exe.win32-3.7/setup.py
+++
@@ -1,9 +0,0 @@
-import cx_Freeze
-
-executables = [cx_Freeze.Executable("main.py")]
-
-cx_Freeze.setup (
-    name = "OpenGLPythonTest",
-    options = {"build_exe": {"packages": ["pygame", "OpenGL", "PIL", "numpy", "pubsub"]}},
-    executables = executables
-    )
